68932f265f1579f32663b5909afd36a72b925655
Sotula Dmytro <sotula.d@gmail.com>
2021-11-11 14:27:57 +0200

    well DONE

--- practice-4/src/task-6.js
+++ practice-4/src/task-6.js
@@ -1,8 +1,29 @@
 
 export default class EnhancedPromise extends Promise {
     static some(promises, count) {
-        // Change me!
+        const rejects = promises.length - count;
+        const errors = [];
+        const result = [];
+        
+        if (rejects < 0) return EnhancedPromise.reject(new Error());
+        if (rejects === 0) return EnhancedPromise.resolve(result);
 
-        return new EnhancedPromise();
+        return new EnhancedPromise((resolve, reject) => {
+            promises.map(prom => {
+                prom.then(
+                    res => {
+                        result.push(res);
+                        if (result.length >= count) {
+                            resolve(result);
+                        }
+                    },
+                    err => {
+                        errors.push(err);
+                        if (errors.length >= rejects) {
+                            reject(new Error());
+                        }
+                    });
+            });
+        });
     }
 }
--- practice-4/src/task-7.js
+++ practice-4/src/task-7.js
@@ -1,7 +1,16 @@
 import { getJSON } from "./task-1.js";
 
 export default function getSequential(urls) {
-    // Change me!
+    const result = [];
 
-    return new Promise();
+    return new Promise((resolve, reject) => {
+        urls.map(url => {
+            getJSON(url).then(response => {
+                result.push(response);
+                if (result.length === urls.length) resolve(result);
+            }).catch(() => {
+                reject(new Error(`failed to fetch ${url}`));
+            });
+        });
+    });
 }
