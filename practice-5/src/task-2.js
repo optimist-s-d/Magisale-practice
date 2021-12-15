74d75c69701e9309984618446eb3d445da082d52
Dima Sotula <sotula.d@gmail.com>
2021-12-15 12:45:14 +0200

    Done int & float is now active

--- practice-5/src/task-2.js
+++ practice-5/src/task-2.js
@@ -1,88 +1,139 @@
-import React from "react";
-import Input from "./Input";
-import Button from "./Button";
+    import React from "react";
+    import Input from "./Input";
+    import Button from "./Button";
 
-class Calculator extends React.Component {
-    constructor(props){
+    class Calculator extends React.Component {
+    constructor(props) {
         super(props);
-        
+
         this.state = {
-            operand1: "",
-            operand2: "",
-            result: ""
+        operand1: "",
+        operand2: "",
+        result: "",
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleClick = this.handleClick.bind(this);
     }
 
-    handleChange(e){
-        const value = e.target.value.replace(/[^0-9]+/g, "");
-        if(e.target.name === "result"){
-            return false;
+    handleChange(e) {
+        const value = e.target.value.replace(/[^\d.-]/g, "");
+        if (e.target.name === "result") {
+        return false;
         }
         this.setState({
-            [e.target.name]: value
+        [e.target.name]: value,
         });
     }
 
-    handleClick(e){
-        let firstValue = parseInt(this.state.operand1);
-        let secondValue = parseInt(this.state.operand2);
-        switch(e.target.name){
-            case "Clear":
-                this.setState({
-                    operand1: 0,
-                    operand2: 0,
-                    result: 0
+    handleClick(e) {
+        let firstValue = Number(this.state.operand1);
+        let secondValue = Number(this.state.operand2);
+        switch (e.target.name) {
+        case "Clear":
+            this.setState({
+            operand1: 0,
+            operand2: 0,
+            result: 0,
+            });
+            break;
+        case "Add":
+            this.setState({
+            result: firstValue + secondValue,
+            });
+            break;
+        case "Subtract":
+            this.setState({
+            result: firstValue - secondValue,
+            });
+            break;
+        case "Multiply":
+            this.setState({
+            result: firstValue * secondValue,
+            });
+            break;
+        case "Divide":
+            (secondValue != 0)
+            ? this.setState({
+                result: firstValue / secondValue,
+                })
+            : this.setState({
+                result: "False",
                 });
-                break;
-            case "Add":
-                this.setState({
-                    result: firstValue + secondValue
-                });
-                break;
-            case "Subtract":
-                this.setState({
-                    result: firstValue - secondValue
-                });
-                break;
-            case "Multiply":
-                this.setState({
-                    result: firstValue*secondValue
-                });
-                break;
-            case "Divide":
-                this.setState({
-                    result: firstValue/secondValue
-                });
-                break;
+            break;
         }
-        
     }
 
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-3"><Input placeholder="Operand 1" name="operand1" onChange={this.handleChange} inputValue={this.state.operand1}/></div>
-                    <div className="col-3"><Input placeholder="Operand 2" name="operand2" onChange={this.handleChange} inputValue={this.state.operand2}/></div>
-                    <div className="col-2"><Button class="btn btn-block btn-danger" name="Clear" onClick={this.handleClick} /></div>
-                </div>
-                <div className="row my-3">
-                    <div className="col-2"><Button class="btn btn-block btn-secondary" name="Add" onClick={this.handleClick} /></div>
-                    <div className="col-2"><Button class="btn btn-block btn-secondary" name="Subtract" onClick={this.handleClick} /></div>
-                    <div className="col-2"><Button class="btn btn-block btn-secondary" name="Multiply" onClick={this.handleClick} /></div>
-                    <div className="col-2"><Button class="btn btn-block btn-secondary" name="Divide" onClick={this.handleClick} /></div>
-                </div>
-                <div className="row">
-                    <div className="col-4">
-                        <Input placeholder="Result" name="result" inputValue={this.state.result} />
-                    </div>
-                </div>
+        <div className="container">
+            <div className="row">
+            <div className="col-3">
+                <Input
+                placeholder="Operand 1"
+                name="operand1"
+                onChange={this.handleChange}
+                inputValue={this.state.operand1}
+                />
             </div>
+            <div className="col-3">
+                <Input
+                placeholder="Operand 2"
+                name="operand2"
+                onChange={this.handleChange}
+                inputValue={this.state.operand2}
+                />
+            </div>
+            <div className="col-2">
+                <Button
+                class="btn btn-block btn-danger"
+                name="Clear"
+                onClick={this.handleClick}
+                />
+            </div>
+            </div>
+            <div className="row my-3">
+            <div className="col-2">
+                <Button
+                class="btn btn-block btn-secondary"
+                name="Add"
+                onClick={this.handleClick}
+                />
+            </div>
+            <div className="col-2">
+                <Button
+                class="btn btn-block btn-secondary"
+                name="Subtract"
+                onClick={this.handleClick}
+                />
+            </div>
+            <div className="col-2">
+                <Button
+                class="btn btn-block btn-secondary"
+                name="Multiply"
+                onClick={this.handleClick}
+                />
+            </div>
+            <div className="col-2">
+                <Button
+                class="btn btn-block btn-secondary"
+                name="Divide"
+                onClick={this.handleClick}
+                />
+            </div>
+            </div>
+            <div className="row">
+            <div className="col-4">
+                <Input
+                placeholder="Result"
+                name="result"
+                inputValue={this.state.result}
+                />
+            </div>
+            </div>
+        </div>
         );
     }
-}
+    }
 
-export default Calculator;
+    export default Calculator;
