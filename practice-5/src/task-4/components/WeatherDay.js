16a9953ae9a578e51e57ec76fd0fe66e3ade62a3
Dima Sotula <sotula.d@gmail.com>
2021-12-15 10:25:56 +0200

    done

--- practice-5/src/task-4/components/WeatherDay.js
+++ practice-5/src/task-4/components/WeatherDay.js
@@ -3,21 +3,38 @@ import React from "react";
 import { connect } from "react-redux";
 import { openDayDetails } from "../actions/day-forecast";
 
-const WeatherDay = props => {
+const WeatherDay = (props) => {
     return (
-        <li className={props.selectedDt === props.day.dt ? "list-inline-item active" : "list-inline-item"} onClick={() => props.openDayDetails(props.day.dt)}>
-            <div className="day-name">{new Date(props.day.dt).toLocaleDateString("en-EN", {weekday: "short"})}</div>
-            <img src={`img/${props.day.weather.icon}.png`} alt={props.day.weather.description} />
-            <div className="temp">{Math.floor(props.day.temp.min)}&#x2103; {Math.floor(props.day.temp.max)}&#x2103;</div>
-        </li>
+        <li
+        className={
+            props.selectedDt === props.day.dt
+            ? "list-inline-item active"
+            : "list-inline-item"
+        }
+        onClick={() => props.openDayDetails(props.day.dt)}
+        >
+        <div className="day-name">
+            {new Date(props.day.dt).toLocaleDateString("en-EN", {
+            weekday: "short",
+            })}
+        </div>
+        <img
+            src={`img/${props.day.weather.icon}.png`}
+            alt={props.day.weather.description}
+        />
+        <div className="temp">
+            {Math.floor(props.day.temp.min)}&#x2103;{" "}
+            {Math.floor(props.day.temp.max)}&#x2103;
+        </div>
+    </li>
     );
 };
 
-const mapStateToProps = state => ({
-    selectedDt: state.selectedDt
+const mapStateToProps = (state) => ({
+    selectedDt: state.selectedDt,
 });
-const mapDispatchToProps = dispatch => ({
-    openDayDetails: (key) => dispatch(openDayDetails(key))
+const mapDispatchToProps = (dispatch) => ({
+    openDayDetails: (key) => dispatch(openDayDetails(key)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(WeatherDay);
